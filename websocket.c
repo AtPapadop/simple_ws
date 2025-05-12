bd11165b2a653d5030206b7d9a79b9b84b4829eb
atpapadop <atpapadop@it.auth.gr>
2025-05-12 19:01:50 +0300

    added more frame creation functions

--- websocket.c
+++ websocket.c
@@ -164,12 +164,6 @@ void ws_create_pong_frame(uint8_t *out_data, size_t *out_len)
     ws_create_frame(&frame, out_data, out_len);
 }
 
-void ws_create_opening_frame(uint8_t *out_data, size_t *out_len)
-{
-    ws_frame frame = {.payload_length = 0, .payload = NULL, .type = WS_OPENING_FRAME};
-    ws_create_frame(&frame, out_data, out_len);
-}
-
 void ws_create_closing_frame(uint8_t *out_data, size_t *out_len)
 {
     ws_frame frame = {.payload_length = 0, .payload = NULL, .type = WS_CLOSING_FRAME};
